ec41dfda10fd22c5c51450f77e41b3e17fb17d7c
Bhuwan Shahi <shahibhuwan5555@gmail.com>
2021-08-19 14:39:53 +0545

    auto saving html document

--- tutorial/spiders/first.py
+++ tutorial/spiders/first.py
@@ -1,9 +1,10 @@
 import scrapy 
 from ..items import TutorialItem
-from scrapy.http.request import Request
+import w3lib.html
 
 class FirstWebsite(scrapy.Spider):
     name="firstwebsite"
+    
     page_number= 1
     start_urls= ["https://traveltriangle.com/tour-packages/nepal/page/1"]
 
@@ -36,8 +37,10 @@ class FirstWebsite(scrapy.Spider):
         products = response.xpath('//div[@class="clearfix row p8 bb radius2"]')
         item=TutorialItem()
         for product in products:
-           # url_inside =product.xpath('div[2]/div/h3/a/@href').get()
-            item['url'] =product.xpath('div[2]/div/h3/a/@href').get() #response.urljoin(url_inside)
+            url_inside =product.xpath('div[2]/div/h3/a/@href').get()
+            
+            item['url'] =response.urljoin(url_inside)#product.xpath('div[2]/div/h3/a/@href').get() 
+            
             yield response.follow(url = item['url'], meta = {'item': item}, callback=self.parse_additional_info) 
         next_page= 'https://traveltriangle.com/tour-packages/nepal/page/'+str(FirstWebsite.page_number)        
         if FirstWebsite.page_number<=3 :
@@ -46,16 +49,28 @@ class FirstWebsite(scrapy.Spider):
             yield response.follow(next_page, callback=self.parse)
 #scrapy.Request() instead of response.follow
     def parse_additional_info(self, response):
+        # page = response.url.split('/')[-1]
+        # filename = f'tours-{page}.html'
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+              
+
+        filename = response.url.split("/")[-1][:13] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
         item = response.meta['item']
         item['name'] = response.xpath('//div[@class="clearfix row"]/div[3]/div/div/h1/span/text()').extract_first().strip()
         #span[@class="iblock mr8"]
        # item['description1'] = response.xpath('//div[@class="clearfix p15 border mt8 pt24"]/div/div[1]/label/h3/text()[2]').extract_first()
-        item['description2']=response.xpath('//div[@class="clearfix p15 border mt8 pt24"]/div/div[1]/div/div/div[2]').extract_first()
-        item['description3']=response.xpath('//div[@class="clearfix p15 border mt8 pt24"]/div/div[2]/div/div/div[2]').extract_first()
-        item['description4'] = response.xpath('//div[@class="clearfix p15 border mt8 pt24"]/div/div[3]/div/div/div[2]').extract_first()
-        item['price'] =response.xpath('//div[@class="pdp-right-block fleft iblock row pl15 ml15"]/div[2]/div/div/div/div/div/h4/span/text()').get()
-        item['types'] = response.xpath('//div[@class="clearfix p15 border"]/div/div[3]/div/span/span[2]/text()').get()
-        item['citi'] = response.xpath('//div[@class="pdp-right-block fleft iblock row pl15 ml15"]/div/div/div/div[2]/div/ul').extract_first()
+        item['day1']=w3lib.html.remove_tags(response.xpath('//div[@class="clearfix p15 border mt8 pt24"]/div/div[1]/label/h3').extract_first())#//div[@class="clearfix p15 border mt8 pt24"]/div/div[1]/div/div/div[2]
+        item['day2']=w3lib.html.remove_tags(response.xpath('//div[@class="clearfix p15 border mt8 pt24"]/div/div[2]/label/h3').extract_first()) #//div[@class="clearfix p15 border mt8 pt24"]/div/div[2]/div/div/div[2]
+        item['day3'] = w3lib.html.remove_tags(response.xpath('//div[@class="clearfix p15 border mt8 pt24"]/div/div[3]/label/h3').extract_first())#//div[@class="clearfix p15 border mt8 pt24"]/div/div[3]/div/div/div[2]
+
+        item['day4']=w3lib.html.remove_tags(response.xpath('//div[@class="clearfix p15 border mt8 pt24"]/div/div[4]/label/h3').extract_first())#//div[@class="clearfix p15 border mt8 pt24"]/div/div[4]/div/div/div[2]
+        item['hotels']= str(response.xpath('//div[@class="hotel-card bb pt15 pb15"] /div/div[2]/div/div/h3/text()').extract_first()) +',' + str(response.xpath('//div[@class="hotel-card bb pt15 pb15"][2]/div/div[2]/div/div/h3/text()').extract_first())
+        item['price'] =w3lib.html.remove_tags(response.xpath('//div[@class="pdp-right-block fleft iblock row pl15 ml15"]/div[2]/div/div/div/div/div/h4/span/text()').get())
+        item['types'] = w3lib.html.remove_tags(response.xpath('//div[@class="clearfix p15 border"]/div/div[3]/div/span/span[2]/text()').get())
+        item['citi'] = w3lib.html.remove_tags(response.xpath('//div[@class="pdp-right-block fleft iblock row pl15 ml15"]/div/div/div/div[2]/div/ul').extract_first())
         # item['discount']=response.xpath('')
         yield item
 
